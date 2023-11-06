85afb1d9b681ab0ccfb0f67348185662d63776ea
Noelia.Ramos <nramos-105111>
2023-11-06 02:03:16 -0300

    tarea M4U3

---
+++ M4U3/app.js
@@ -0,0 +1,57 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var ContactoRouter = require('./routes/Contacto'); //ruta de contacto
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/Contacto',ContactoRouter);
+
+///tarea
+app.get('/noticias',function(req,res){
+  res.send('Este es el portal principal de noticias')
+})
+
+app.get('/clima',function(req,res){
+  res.send('Este es el portal de clima')
+})
+
+app.get('/horoscopo',function(req,res){
+  res.send('Este es el portal del horoscopo')
+})
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
